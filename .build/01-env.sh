6073bfc749b8625a3d402887b01a705722cba77c
Douglas Gadêlha <dgadelha@users.noreply.github.com>
2016-11-15 15:04:20 -0300

    Show repo sync progress

--- .build/01-env.sh
+++ .build/01-env.sh
@@ -25,71 +25,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-RED="\033[31;1m"
-GREEN="\033[32;1m"
-RESET="\033[0m"
-
-travis_wait() {
-  local timeout=$1
-
-  if [[ $timeout =~ ^[0-9]+$ ]]; then
-    # looks like an integer, so we assume it's a timeout
-    shift
-  else
-    # default value
-    timeout=20
-  fi
-
-  local cmd="$@"
-  local log_file=travis_wait_$$.log
-
-  $cmd &>$log_file &
-  local cmd_pid=$!
-
-  travis_jigger $! $timeout $cmd &
-  local jigger_pid=$!
-  local result
-
-  {
-    wait $cmd_pid 2>/dev/null
-    result=$?
-    ps -p$jigger_pid &>/dev/null && kill $jigger_pid
-  } || return 1
-
-  if [ $result -eq 0 ]; then
-    echo -e "\n${GREEN}The command \"$TRAVIS_CMD\" exited with $result.${RESET}"
-  else
-    echo -e "\n${RED}The command \"$TRAVIS_CMD\" exited with $result.${RESET}"
-  fi
-
-  echo -e "\n${GREEN}Log:${RESET}\n"
-  cat $log_file
-
-  return $result
-}
-
-travis_jigger() {
-  # helper method for travis_wait()
-  local cmd_pid=$1
-  shift
-  local timeout=$1 # in minutes
-  shift
-  local count=0
-
-
-  # clear the line
-  echo -e "\n"
-
-  while [ $count -lt $timeout ]; do
-    count=$(($count + 1))
-    echo -ne "Still running ($count of $timeout): $@\r"
-    sleep 60
-  done
-
-  echo -e "\n${RED}Timeout (${timeout} minutes) reached. Terminating \"$@\"${RESET}\n"
-  kill -9 $cmd_pid
-}
-
 set -ev
 git config --global user.name "Travis CI"
 git config --global user.email "dgadelha@users.noreply.github.com"
@@ -97,4 +32,4 @@ git config --global color.ui "true"
 
 repo init -u https://android.googlesource.com/platform/manifest -b android-6.0.1_r24 > /dev/null 2>&1
 mv -fv local_manifests .repo
-travis_wait 45 "repo sync -c -j16 > /dev/null 2>&1"
+repo sync -c -j32 > /dev/null
